5c195657da30f73bb0ab0abb499cae6694299060
zhouli <kangaoxiaoshi@163.com>
2016-09-10 09:48:01 +0800

    1. index 页面添加功能

--- asset/src/controller/index.js
+++ asset/src/controller/index.js
@@ -9,12 +9,15 @@ define([
 		var View = pageView.extend({
 			onCreate: function () {
 				var html = this.template(template, {});
-				debugger;
 				this.$el.html(html);
 			},
 
 			events: {
 				'click .js-alert': 'alertAction'
+			},
+		
+			onShow: function (data) {
+				
 			},
 			alertAction: function () {
 				alert('ok');
--- asset/src/h5/widget/lazy.js
+++ asset/src/h5/widget/lazy.js
@@ -60,11 +60,60 @@ define([
       });
       app.currentPage = view;
       //这里处理你的逻辑，比如 动画什么的
-      debugger;
+      
       this.main.append(view.el);
-    }
+      app.loadPage(view);
+      this.firstLoad = false;
+    },
     
   });
+
+  var app = {
+    loadPage: function (view) {
+     
+      var ajaxs = view.ajax ? view.ajax() : null;
+      if (_.isArray(ajaxs)) {
+        var count = 0,
+          result = [];
+        _.each(ajaxs, function(item, index) {
+          item.success = function(data) {
+            result[index] = data;
+            if (++count >= ajaxs.length) {
+              view.onShow.apply(view, result);
+            }
+          };
+          app.ajax(item);
+        })
+      } else if (ajaxs) {
+        ajaxs.success = function (data, status, xhr) {
+          view.onShow.apply(view, result);
+        }
+        app.ajax(ajaxs);
+      }
+    },
+    ajax: function (params) {
+      if (!params.data) {
+        params.data = {};
+      }
+      params.url = config.gateway + params.url;
+      params.type = params.type || 'post';
+      params.timeout =  params.timeout || 10000;
+      params.success = function(data, status, xhr) { 
+        if (params.success) {
+          params.success(data, status, xhr, options.data);
+        }
+      },
+      params.error = function(xhr, errorType, error) {
+        if (params.error) {
+          params.error();
+        } else {
+          app.alert('网络出错，请稍后再试');
+        }
+      }
+
+      return $.ajax(params);
+    }
+  }; 
 
   return lazy;
 })
